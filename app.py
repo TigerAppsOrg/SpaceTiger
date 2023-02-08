6bef8ade1c5c018d23bf31933c75a433902f5fd3
Adam Kelch <askelch@gmail.com>
2023-02-07 21:47:45 -0500

    a

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def user_logged_in():
 # ----------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
