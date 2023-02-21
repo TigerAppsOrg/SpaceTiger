531dcba8c890802a06057233476537099252cf42
ysl-ee <113142488+ysl-ee@users.noreply.github.com>
2023-02-21 16:12:37 -0500

    use landing image

--- frontend/src/routes/Landing.jsx
+++ frontend/src/routes/Landing.jsx
@@ -168,7 +168,7 @@ export default function Landing() {
             className={classes.image}
             height="100%"
             alt="A tiger in space"
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2Fbddf6686-6128-408a-a2a1-09341001a276%2Fd9n18w1-5f8c062d-d0cc-4dac-b46f-ed826bab3249.jpg%2Fv1%2Ffill%2Fw_1032%2Ch_774%2Cq_70%2Cstrp%2Fspace_tiger_by_goldenspiraldesigns_d9n18w1-pre.jpg%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD04NTAwIiwicGF0aCI6IlwvZlwvYmRkZjY2ODYtNjEyOC00MDhhLWEyYTEtMDkzNDEwMDFhMjc2XC9kOW4xOHcxLTVmOGMwNjJkLWQwY2MtNGRhYy1iNDZmLWVkODI2YmFiMzI0OS5qcGciLCJ3aWR0aCI6Ijw9MTEzMzMifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.z0Tf0rx_NtzOE7jUOH9pKXGJHc6MIRUJCmkhChSVqcM&f=1&nofb=1&ipt=abca2a18804d2dc6a34f25905844a642b3c6764e1791377af922f07a55726ebe&ipo=images"
+            src="/frontend/src/routes/landing.jpg"
           />
         </Grid>
       </Grid>
